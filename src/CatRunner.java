da02d0a09118c074091d7752fb2e0dccf1f75f01
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-28 17:00:58 -0800

    An epic car race and a meowing cat.

---
+++ src/CatRunner.java
@@ -0,0 +1,9 @@
+
+public class CatRunner {
+	public static void main(String[] args) {
+		Cat Barbara= new Cat("Barbara");
+		Barbara.meow();
+	}
+
+
+}
--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -7,14 +7,26 @@ public class RaceCarRunner {
 		// 1. Create a RaceCar and place it in 5th position
 	RaceCar Best=new RaceCar("HotWheels", 5);
 		// 2. Print the RaceCar's position in the race
- Best.getPositionInRace();
- System.out.println(getPositionInRace);
+ int position=Best.getPositionInRace();
+ 
+ System.out.println("Your car is currently number "+position+" in the race.");
 
 		// 3. Crash the RaceCar
-		
+		Best.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if(Best.damaged==true) {
+	Best.pit();
+}
 		// 5. Help the car move into first place.
+System.out.println("Your newly repaired car is zooming past the others.");
+
+while(Best.getPositionInRace()>1) {
+	Best.overtake();
+	
+}
+if(Best.getPositionInRace()==1) {
+	System.out.println("You win!");
+}
 
 	}
 }
